9feeda5226419ed6d0cef2aca05039197b734413
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-04-06 18:55:07 +0530

    2 files modified

--- app/instructions/instructions.component.ts
+++ app/instructions/instructions.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
-  styleUrls: ['./instructions.component.css']
+  styleUrls: ['./instructions.component.css'],
 })
 export class InstructionsComponent implements OnInit {
+  outletHTML = '<router-outlet></router-outlet>';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
