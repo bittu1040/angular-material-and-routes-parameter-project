d3d15e144f04e54a2c5631b6d2cecede08193a8a
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-04-06 10:53:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-mini-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-example-achne1)
---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  ngOnInit() {}
+
+  fruitsData = [
+    { name: 'gauva', quantity: 2, img: 'bbbbb' },
+    { name: 'mango', quantity: 3, img: 'bbbbb' },
+    { name: 'papaya', quantity: 4, img: 'bbbbb' },
+  ];
+
+  showCard : any;
+
+  showDetails(button) {
+    this.showCard = button ;
+    console.log(this.showCard)
+  }
+}
